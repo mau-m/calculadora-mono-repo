992a90903948608d0f67af4c1265fe7ed120fa6c
Mauricio Miranda <mirandabmau@gmail.com>
2026-05-04 23:20:55 -0600

    corregir errores

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
+
+# Instala dependencias de sistema necesarias
+apt-get update
+apt-get install -y \
+  aws-cli \
+  docker.io \
+  python3-pip
+
+# Limpia el directorio de aplicación anterior
 rm -rf /opt/mi-app
 mkdir -p /opt/mi-app
